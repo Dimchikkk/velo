310db1c5a423b8ded71ba08745a8ba4034820987
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-03-10 16:38:50 +0000

    small refactoring
    Former-commit-id: 8046cc4e5ebb7beee63f1ccf6f1d7d06062cd27a

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,20 @@ pub use hello_plugin::*;
 
 fn main() {
     App::new()
+        .add_startup_system(setup_background)
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
         .run();
+}
+
+fn setup_background(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
+    let background_image = asset_server.load("bg.png");
+    commands.spawn((Camera2dBundle::default(), MainCamera));
+    commands.spawn(SpriteBundle {
+        texture: background_image,
+        ..Default::default()
+    });
 }
