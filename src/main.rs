552a7d744b25342fc5fc93b760484c2ee476f512
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-03-11 16:59:05 +0000

    full screen on wasm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use bevy::prelude::*;
+use bevy::{prelude::*,window::PresentMode};
 mod hello_plugin;
 pub use hello_plugin::*;
 
 fn main() {
     App::new()
         .add_startup_system(setup_background)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Interactive Chart".into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(HelloPlugin)
         .run();
 }
