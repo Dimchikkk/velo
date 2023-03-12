a128a491d5728980b395d80535f31016d43a0dd3
StaffEngineer <velo.app1@gmail.com>
2023-03-12 01:21:53 +0000

    allow moving screenshots
    Former-commit-id: 0aebcc5e6155a6f2377a8ce5934fb28976134ec1

---
+++ src/hello_plugin/structs.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct MainCamera;
+
+#[derive(Component)]
+pub struct IRectangle {
+    pub id: u32,
+}
+
+#[derive(Component)]
+pub struct CreateRectButton;
+
+#[derive(Component)]
+pub struct EditableText {
+    pub id: u32,
+}
+
+#[derive(Component)]
+pub struct Top {
+    pub id: u32,
+}
+pub struct AddRect;
+
+#[derive(Resource, Default)]
+pub struct AppState {
+    pub focused_id: Option<u32>,
+    pub entity_counter: u32,
+}
