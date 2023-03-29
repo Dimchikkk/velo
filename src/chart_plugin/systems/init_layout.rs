82a848b09b72024f5fee030eeb07d92e09c23f93
StaffEngineer <velo.app1@gmail.com>
2023-03-29 20:52:44 +0100

    update readme
    Former-commit-id: 380d2321a995fd2f740c533de4cfa723d3fcc895

--- src/chart_plugin/systems/init_layout.rs
+++ src/chart_plugin/systems/init_layout.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
+use bevy_ui_borders::BorderColor;
 
 use crate::{AppState, MainCamera, SaveRequest};
 
 use super::ui_helpers::{
-    add_rectangle_txt, create_rectangle_txt, CreateRectButton, DelRectButton, LeftPanel,
-    LeftPanelControls, LeftPanelExplorer, LoadState, MainPanel, Menu, Root, SaveState,
+    add_rectangle_txt, create_rectangle_txt, ChangeColor,
+    LeftPanel, LeftPanelControls, LeftPanelExplorer, LoadState, MainPanel, Menu, Root, SaveState,  ButtonAction, self,
 };
 
 pub fn init_layout(
@@ -170,7 +171,7 @@ pub fn init_layout(
         .spawn((
             NodeBundle {
                 style: Style {
-                    size: Size::new(Val::Percent(100.), Val::Percent(30.)),
+                    size: Size::new(Val::Percent(100.), Val::Percent(20.)),
                     flex_direction: FlexDirection::Column,
                     align_items: AlignItems::Center,
                     justify_content: JustifyContent::Center,
@@ -185,7 +186,7 @@ pub fn init_layout(
         .spawn((
             NodeBundle {
                 style: Style {
-                    size: Size::new(Val::Percent(100.), Val::Percent(70.)),
+                    size: Size::new(Val::Percent(100.), Val::Percent(80.)),
                     align_items: AlignItems::Center,
                     justify_content: JustifyContent::Center,
                     ..default()
@@ -199,57 +200,67 @@ pub fn init_layout(
     commands.entity(left_panel).add_child(left_panel_controls);
     commands.entity(left_panel).add_child(left_panel_explorer);
 
-    let add_rect = commands
-        .spawn((
-            ButtonBundle {
-                background_color: Color::rgb(0.8, 0.8, 0.8).into(),
-                style: Style {
-                    size: Size::new(Val::Percent(50.), Val::Percent(30.)),
-                    align_items: AlignItems::Center,
-                    margin: UiRect {
-                        left: Val::Px(5.),
-                        right: Val::Px(5.),
-                        top: Val::Px(5.),
-                        bottom: Val::Px(5.),
-                    },
-                    justify_content: JustifyContent::Center,
-                    ..default()
+    let creation = add_two_buttons(
+        &mut commands, 
+        font.clone(), 
+        "NEW".to_string(),
+        "DELETE".to_string(), 
+        ButtonAction { button_type: ui_helpers::ButtonTypes::ADD },
+        ButtonAction { button_type: ui_helpers::ButtonTypes::DEL }
+    );
+    let z_index = add_two_buttons(
+        &mut commands, 
+        font.clone(), 
+        "FRONT".to_string(),
+        "BACK".to_string(), 
+        ButtonAction { button_type: ui_helpers::ButtonTypes::FRONT },
+        ButtonAction { button_type: ui_helpers::ButtonTypes::BACK }
+    );
+    let tagging = add_two_buttons(
+        &mut commands, 
+        font.clone(), 
+        "TAG".to_string(),
+        "UNTAG".to_string(), 
+        ButtonAction { button_type: ui_helpers::ButtonTypes::TAG },
+        ButtonAction { button_type: ui_helpers::ButtonTypes::UNTAG }
+    );
+    
+    let color_picker = commands
+        .spawn((NodeBundle {
+            style: Style {
+                align_items: AlignItems::Center,
+                size: Size::new(Val::Percent(80.), Val::Percent(20.)),
+                margin: UiRect {
+                    left: Val::Px(5.),
+                    right: Val::Px(5.),
+                    top: Val::Px(5.),
+                    bottom: Val::Px(5.),
                 },
+                justify_content: JustifyContent::Center,
                 ..default()
             },
-            CreateRectButton,
-        ))
-        .with_children(|builder| {
-            builder.spawn(add_rectangle_txt(font.clone(), "NEW".to_string()));
-        })
+            ..default()
+        },))
         .id();
+    let color1 = add_color(
+        &mut commands,
+        Color::rgb(251. / 255., 232. / 255., 166. / 255.),
+    );
+    let color2 = add_color(&mut commands, Color::WHITE);
+    let color3 = add_color(&mut commands, Color::RED);
+    let color4 = add_color(&mut commands, Color::GREEN);
+    let color5 = add_color(&mut commands, Color::GRAY);
 
-    let del_rect = commands
-        .spawn((
-            ButtonBundle {
-                background_color: Color::rgb(0.8, 0.8, 0.8).into(),
-                style: Style {
-                    size: Size::new(Val::Percent(50.), Val::Percent(30.)),
-                    align_items: AlignItems::Center,
-                    margin: UiRect {
-                        left: Val::Px(5.),
-                        right: Val::Px(5.),
-                        top: Val::Px(5.),
-                        bottom: Val::Px(5.),
-                    },
-                    justify_content: JustifyContent::Center,
-                    ..default()
-                },
-                ..default()
-            },
-            DelRectButton,
-        ))
-        .with_children(|builder| {
-            builder.spawn(add_rectangle_txt(font.clone(), "DEL".to_string()));
-        })
-        .id();
-    commands.entity(left_panel_controls).add_child(add_rect);
-    commands.entity(left_panel_controls).add_child(del_rect);
+    commands.entity(color_picker).add_child(color1);
+    commands.entity(color_picker).add_child(color2);
+    commands.entity(color_picker).add_child(color3);
+    commands.entity(color_picker).add_child(color4);
+    commands.entity(color_picker).add_child(color5);
+
+    commands.entity(left_panel_controls).add_child(creation);
+    commands.entity(left_panel_controls).add_child(z_index);
+    commands.entity(left_panel_controls).add_child(tagging);
+    commands.entity(left_panel_controls).add_child(color_picker);
 
     commands.entity(main_bottom).add_child(left_panel);
     commands.entity(main_bottom).add_child(main_panel);
@@ -257,4 +268,97 @@ pub fn init_layout(
     commands.entity(root_ui).add_child(main_bottom);
 
     state.main_panel = Some(main_panel);
+}
+
+fn add_color(commands: &mut Commands, color: Color) -> Entity {
+    commands
+        .spawn((
+            ButtonBundle {
+                background_color: color.into(),
+                style: Style {
+                    size: Size::new(Val::Percent(15.), Val::Percent(100.)),
+                    align_items: AlignItems::Center,
+                    margin: UiRect {
+                        left: Val::Px(5.),
+                        right: Val::Px(5.),
+                        top: Val::Px(5.),
+                        bottom: Val::Px(5.),
+                    },
+                    border: UiRect::all(Val::Px(1.)),
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            BorderColor(Color::BLACK),
+            ChangeColor { color },
+        ))
+        .id()
+}
+
+fn add_two_buttons(
+    commands: &mut Commands, 
+    font: Handle<Font>, 
+    label_do: String, 
+    label_undo: String, 
+    component_do: impl Component,
+    component_undo: impl Component,
+) -> Entity {
+        let node = commands
+        .spawn(NodeBundle {
+            style: Style {
+                align_items: AlignItems::Center,
+                size: Size::new(Val::Percent(85.), Val::Percent(15.)),
+                margin: UiRect {
+                    left: Val::Px(5.),
+                    right: Val::Px(5.),
+                    top: Val::Px(5.),
+                    bottom: Val::Px(5.),
+                },
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            ..default()
+        })
+        .id();
+    let do_button = commands
+        .spawn((
+            ButtonBundle {
+                background_color: Color::rgb(0.8, 0.8, 0.8).into(),
+                style: Style {
+                    size: Size::new(Val::Percent(40.), Val::Percent(100.)),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            component_do
+        ))
+        .with_children(|builder| {
+            builder.spawn(add_rectangle_txt(font.clone(), label_do));
+        })
+        .id();
+
+    let undo_button = commands
+        .spawn((
+            ButtonBundle {
+                background_color: Color::rgb(0.8, 0.8, 0.8).into(),
+                style: Style {
+                    align_items: AlignItems::Center,
+                    size: Size::new(Val::Percent(40.), Val::Percent(100.)),
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            component_undo
+        ))
+        .with_children(|builder| {
+            builder.spawn(add_rectangle_txt(font.clone(), label_undo));
+        })
+        .id();
+    commands.entity(node).add_child(do_button);
+    commands.entity(node).add_child(undo_button);
+    node
 }
