24c5e5441c7334a20b445d470c500f06c0de2229
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-04-01 11:46:19 +0100

    move componenets to separate file
    Former-commit-id: eb15f64a41364f39c70af31b24abfbe860d98fc9

--- src/chart_plugin/ui_helpers/ui_helpers.rs
+++ src/chart_plugin/ui_helpers/ui_helpers.rs
@@ -1,6 +1,6 @@
 use bevy_ui_borders::{BorderColor, Outline};
 use linkify::{LinkFinder, LinkKind};
-use serde::{Deserialize, Serialize};
+
 use std::f32::consts::PI;
 
 use bevy::{prelude::*, text::BreakLineOn};
@@ -8,190 +8,12 @@ use bevy_prototype_lyon::{
     prelude::{GeometryBuilder, ShapeBundle, Stroke},
     shapes,
 };
-use uuid::Uuid;
 
 use crate::TextPos;
 
-#[derive(Component)]
-pub struct Root;
-
-#[derive(Component)]
-pub struct Menu;
-
-#[derive(Component)]
-pub struct AddTab;
-
-#[derive(Component)]
-pub struct DeleteTab;
-
-#[derive(Component)]
-pub struct RenameTab;
-
-#[derive(Component)]
-pub struct SelectedTab {
-    pub id: ReflectableUuid,
-}
-
-#[derive(Component)]
-pub struct SelectedTabTextInput {
-    pub id: ReflectableUuid,
-}
-
-#[derive(Component)]
-pub struct ChangeColor {
-    pub color: Color,
-}
-
-#[derive(Serialize, Deserialize, Default, Copy, Clone, Reflect, Debug, Eq, PartialEq, Hash)]
-pub enum ArrowType {
-    Line,
-    #[default]
-    Arrow,
-    DoubleArrow,
-    ParallelLine,
-    ParallelArrow,
-    ParallelDoubleArrow,
-}
-
-#[derive(Component)]
-pub struct ArrowMode {
-    pub arrow_type: ArrowType,
-}
-
-#[derive(Component)]
-pub struct TextPosMode {
-    pub text_pos: TextPos,
-}
-
-#[derive(Component)]
-pub enum TextManipulation {
-    Cut,
-    Paste,
-    Copy,
-    OpenAllLinks
-}
-
-#[derive(Component)]
-pub struct SaveState;
-
-#[derive(Component)]
-pub struct LoadState;
-
-#[derive(Component)]
-pub struct MainPanel;
-
-#[derive(Component)]
-pub struct BottomPanel;
-
-#[derive(Component)]
-pub struct LeftPanel;
-
-#[derive(Component)]
-pub struct LeftPanelControls;
-
-#[derive(Component)]
-pub struct LeftPanelExplorer;
-
-#[derive(Eq, PartialEq, Hash, Debug, Clone, Copy)]
-pub struct CreateArrow {
-    pub arrow_type: ArrowType,
-    pub start: ArrowConnect,
-    pub end: ArrowConnect,
-}
-
-#[derive(Clone, Reflect, Default, Debug, Copy, Eq, PartialEq, Hash, Serialize, Deserialize)]
-#[reflect_value]
-pub struct ReflectableUuid(pub Uuid);
-
-#[derive(Component, Default, Reflect, Debug)]
-#[reflect(Component)]
-pub struct Rectangle {
-    pub id: ReflectableUuid,
-}
-
-#[derive(PartialEq, Eq)]
-pub enum ButtonTypes {
-    Add,
-    Del,
-    Front,
-    Back,
-}
-#[derive(Component)]
-pub struct ButtonAction {
-    pub button_type: ButtonTypes,
-}
-
-#[derive(Component, Default, Reflect)]
-#[reflect(Component)]
-pub struct EditableText {
-    pub id: ReflectableUuid,
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Reflect, Default, Serialize, Deserialize)]
-pub enum ArrowConnectPos {
-    #[default]
-    Top,
-    Bottom,
-    Left,
-    Right,
-}
-
-#[derive(
-    Component, Copy, Clone, Debug, Eq, PartialEq, Hash, Reflect, Default, Serialize, Deserialize,
-)]
-#[reflect(Component)]
-pub struct ArrowConnect {
-    pub id: ReflectableUuid,
-    pub pos: ArrowConnectPos,
-}
-
-#[derive(Component, Copy, Clone, Debug, Reflect, Default)]
-#[reflect(Component)]
-pub enum ResizeMarker {
-    #[default]
-    TopLeft,
-    TopRight,
-    BottomLeft,
-    BottomRight,
-}
-
-#[derive(
-    Component, Copy, Clone, Debug, Eq, PartialEq, Hash, Reflect, Default, Serialize, Deserialize,
-)]
-#[reflect(Component)]
-pub struct ArrowMeta {
-    pub arrow_type: ArrowType,
-    pub start: ArrowConnect,
-    pub end: ArrowConnect,
-}
-
-#[derive(Component, Default)]
-pub struct PathModalTop {
-    pub id: ReflectableUuid,
-}
-
-#[derive(Component, Default)]
-pub struct PathModalText {
-    pub id: ReflectableUuid,
-    pub save: bool,
-}
-
-#[derive(Component, Default)]
-pub struct PathModalTextInput {
-    pub id: ReflectableUuid,
-    pub save: bool,
-}
-
-#[derive(Component, Default)]
-pub struct PathModalConfirm {
-    pub id: ReflectableUuid,
-    pub save: bool,
-}
-
-#[derive(Component, Default)]
-pub struct PathModalCancel {
-    pub id: ReflectableUuid,
-}
+#[path = "components.rs"]
+mod components;
+pub use components::*;
 
 fn get_marker_style(position: UiRect, size: f32) -> Style {
     Style {
@@ -308,14 +130,14 @@ pub fn get_sections(text: String, font: Handle<Font>) -> Vec<TextSection> {
         color: Color::BLACK,
     };
     let link_style = TextStyle {
-        font: font.clone(),
+        font,
         font_size: 18.0,
         color: Color::BLUE,
     };
     let mut finder = LinkFinder::new();
     finder.kinds(&[LinkKind::Url]);
     let links: Vec<_> = finder.links(&text).collect();
-    if links.len() == 0 {
+    if links.is_empty() {
         return vec![
             (TextSection {
                 value: text,
@@ -343,7 +165,7 @@ pub fn get_sections(text: String, font: Handle<Font>) -> Vec<TextSection> {
     if idx < text.len() {
         sections.push(TextSection {
             value: text[idx..text.len()].to_string(),
-            style: text_style.clone(),
+            style: text_style,
         });
     }
     sections
