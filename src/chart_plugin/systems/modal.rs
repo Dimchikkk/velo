8c72600e9e0f777a091e2647b77d4848761b5047
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-04-21 00:27:11 +0100

    refactor modal logic
    Former-commit-id: 5a202e2c5a48f36be8ea1212af61bb458de159c0

--- src/chart_plugin/systems/modal.rs
+++ src/chart_plugin/systems/modal.rs
@@ -31,6 +31,63 @@ pub fn cancel_modal(
     }
 }
 
+fn delete_doc(
+    app_state: &mut ResMut<AppState>,
+    commands: &mut Commands,
+    pkv: &mut ResMut<PkvStore>,
+    query_container: &mut Query<(Entity, &DocListItemContainer), With<DocListItemContainer>>,
+) {
+    let current_document = app_state.current_document.unwrap();
+    let id_to_remove = current_document;
+    for (entity, button) in query_container.iter_mut() {
+        if button.id == id_to_remove {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+    app_state.docs.remove(&current_document);
+    for (_, button) in query_container.iter_mut() {
+        if button.id != id_to_remove {
+            app_state.current_document = Some(button.id);
+            break;
+        }
+    }
+    commands.insert_resource(LoadRequest {
+        doc_id: None,
+        drop_last_checkpoint: false,
+    });
+    remove_from_storage(pkv, id_to_remove, app_state.current_document.unwrap());
+}
+
+fn delete_tab(app_state: &mut ResMut<AppState>, commands: &mut Commands) {
+    let current_document = app_state.current_document.unwrap();
+    let index = app_state
+        .docs
+        .get_mut(&current_document)
+        .unwrap()
+        .tabs
+        .iter()
+        .position(|x| x.is_active)
+        .unwrap();
+    app_state
+        .docs
+        .get_mut(&current_document)
+        .unwrap()
+        .tabs
+        .remove(index);
+    let mut last_tab = app_state
+        .docs
+        .get_mut(&current_document)
+        .unwrap()
+        .tabs
+        .last_mut()
+        .unwrap();
+    last_tab.is_active = true;
+    commands.insert_resource(LoadRequest {
+        doc_id: None,
+        drop_last_checkpoint: false,
+    });
+}
+
 pub fn confirm_modal(
     mut commands: Commands,
     mut interaction_query: Query<
@@ -42,155 +99,51 @@ pub fn confirm_modal(
     query_top: Query<(Entity, &ModalTop), With<ModalTop>>,
     mut query_container: Query<(Entity, &DocListItemContainer), With<DocListItemContainer>>,
     mut pkv: ResMut<PkvStore>,
+    input: Res<Input<KeyCode>>,
 ) {
     for (interaction, path_modal_confirm) in interaction_query.iter_mut() {
         if let Interaction::Clicked = interaction {
             for (entity, path_modal_top) in query_top.iter() {
                 if path_modal_confirm.id == path_modal_top.id {
-                    let current_document = app_state.current_document.unwrap();
                     match path_modal_confirm.action {
                         ModalAction::SaveToFile => {}
                         ModalAction::LoadFromFile => {}
                         ModalAction::LoadFromUrl => {}
                         ModalAction::DeleteDocument => {
-                            let id_to_remove = current_document;
-                            for (entity, button) in query_container.iter_mut() {
-                                if button.id == id_to_remove {
-                                    commands.entity(entity).despawn_recursive();
-                                }
-                            }
-                            app_state.docs.remove(&current_document);
-                            for (_, button) in query_container.iter_mut() {
-                                if button.id != id_to_remove {
-                                    app_state.current_document = Some(button.id);
-                                    break;
-                                }
-                            }
-                            commands.insert_resource(LoadRequest {
-                                doc_id: None,
-                                drop_last_checkpoint: false,
-                            });
-                            remove_from_storage(
+                            delete_doc(
+                                &mut app_state,
+                                &mut commands,
                                 &mut pkv,
-                                id_to_remove,
-                                app_state.current_document.unwrap(),
+                                &mut query_container,
                             );
-
-                            commands.entity(entity).despawn_recursive();
-                            ui_state.modal_id = None;
                         }
-                        ModalAction::DeleteTab => {
-                            let index = app_state
-                                .docs
-                                .get_mut(&current_document)
-                                .unwrap()
-                                .tabs
-                                .iter()
-                                .position(|x| x.is_active)
-                                .unwrap();
-                            app_state
-                                .docs
-                                .get_mut(&current_document)
-                                .unwrap()
-                                .tabs
-                                .remove(index);
-                            let mut last_tab = app_state
-                                .docs
-                                .get_mut(&current_document)
-                                .unwrap()
-                                .tabs
-                                .last_mut()
-                                .unwrap();
-                            last_tab.is_active = true;
-                            commands.insert_resource(LoadRequest {
-                                doc_id: None,
-                                drop_last_checkpoint: false,
-                            });
-                            commands.entity(entity).despawn_recursive();
-                            ui_state.modal_id = None;
-                        }
+                        ModalAction::DeleteTab => delete_tab(&mut app_state, &mut commands),
                     }
                 }
+                commands.entity(entity).despawn_recursive();
+                ui_state.modal_id = None;
             }
         }
     }
-}
-
-// TODO remove duplication with confirm_modal function
-pub fn modal_keyboard_input_system(
-    mut app_state: ResMut<AppState>,
-    mut ui_state: ResMut<UiState>,
-    input: Res<Input<KeyCode>>,
-    query_top: Query<(Entity, &ModalTop), With<ModalTop>>,
-    mut commands: Commands,
-    mut query_container: Query<(Entity, &DocListItemContainer), With<DocListItemContainer>>,
-    mut pkv: ResMut<PkvStore>,
-) {
     if input.just_pressed(KeyCode::Return) {
         for (entity, path_modal_top) in query_top.iter() {
             if Some(path_modal_top.id) == ui_state.modal_id {
-                let current_document = app_state.current_document.unwrap();
                 match path_modal_top.action {
                     ModalAction::SaveToFile => todo!(),
                     ModalAction::LoadFromFile => todo!(),
                     ModalAction::LoadFromUrl => todo!(),
                     ModalAction::DeleteDocument => {
-                        let id_to_remove = current_document;
-                        for (entity, button) in query_container.iter_mut() {
-                            if button.id == id_to_remove {
-                                commands.entity(entity).despawn_recursive();
-                            }
-                        }
-                        app_state.docs.remove(&current_document);
-                        for (_, button) in query_container.iter_mut() {
-                            if button.id != id_to_remove {
-                                app_state.current_document = Some(button.id);
-                                break;
-                            }
-                        }
-                        commands.insert_resource(LoadRequest {
-                            doc_id: None,
-                            drop_last_checkpoint: false,
-                        });
-                        remove_from_storage(
+                        delete_doc(
+                            &mut app_state,
+                            &mut commands,
                             &mut pkv,
-                            id_to_remove,
-                            app_state.current_document.unwrap(),
+                            &mut query_container,
                         );
-                        commands.entity(entity).despawn_recursive();
-                        ui_state.modal_id = None;
                     }
-                    ModalAction::DeleteTab => {
-                        let index = app_state
-                            .docs
-                            .get_mut(&current_document)
-                            .unwrap()
-                            .tabs
-                            .iter()
-                            .position(|x| x.is_active)
-                            .unwrap();
-                        app_state
-                            .docs
-                            .get_mut(&current_document)
-                            .unwrap()
-                            .tabs
-                            .remove(index);
-                        let mut last_tab = app_state
-                            .docs
-                            .get_mut(&current_document)
-                            .unwrap()
-                            .tabs
-                            .last_mut()
-                            .unwrap();
-                        last_tab.is_active = true;
-                        commands.insert_resource(LoadRequest {
-                            doc_id: None,
-                            drop_last_checkpoint: false,
-                        });
-                        commands.entity(entity).despawn_recursive();
-                        ui_state.modal_id = None;
-                    }
+                    ModalAction::DeleteTab => delete_tab(&mut app_state, &mut commands),
                 }
+                commands.entity(entity).despawn_recursive();
+                ui_state.modal_id = None;
             }
         }
     }
