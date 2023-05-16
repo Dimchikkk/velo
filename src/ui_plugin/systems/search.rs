80cd9890c18c5c28be3e95246e9a2df184c8f605
StaffEngineer <velo.app1@gmail.com>
2023-05-16 10:34:18 +0100

    no need for clear node index
    Former-commit-id: de41c37bc6c15bd30dc3100324120d86c2ddc9b8

--- src/ui_plugin/systems/search.rs
+++ src/ui_plugin/systems/search.rs
@@ -7,7 +7,8 @@ use tantivy::schema::*;
 use tantivy::Index;
 use uuid::Uuid;
 
-pub struct NodeSearchMeta {
+#[derive(Eq, PartialEq, Hash)]
+pub struct NodeSearchLocation {
     pub doc_id: Uuid,
     pub tab_id: Uuid,
     pub node_id: Uuid,
@@ -25,7 +26,7 @@ pub fn initialize_search_index(dir: PathBuf) -> tantivy::Index {
     })
 }
 
-pub fn update_search_index(index: &Index, meta: NodeSearchMeta, text: &str) -> tantivy::Result<()> {
+pub fn update_search_index(index: &Index, meta: NodeSearchLocation, text: &str) -> tantivy::Result<()> {
     let mut index_writer = index.writer(50_000_000)?;
 
     let term = tantivy::Term::from_field_text(
@@ -58,20 +59,6 @@ pub fn update_search_index(index: &Index, meta: NodeSearchMeta, text: &str) -> t
 
 const MAX_SEARCH_RESULTS: usize = 1000;
 
-pub fn clear_node_index(index: &Index, node_id: &Uuid) -> tantivy::Result<()> {
-    let mut index_writer = index.writer(50_000_000)?;
-
-    let term = tantivy::Term::from_field_text(
-        index.schema().get_field("node_id").unwrap(),
-        &node_id.to_string(),
-    );
-    index_writer.delete_term(term);
-
-    index_writer.commit()?;
-
-    Ok(())
-}
-
 pub fn clear_tab_index(index: &Index, tab_id: &Uuid) -> tantivy::Result<()> {
     let mut index_writer = index.writer(50_000_000)?;
 
@@ -100,7 +87,7 @@ pub fn clear_doc_index(index: &Index, tab_id: &Uuid) -> tantivy::Result<()> {
     Ok(())
 }
 
-pub fn fuzzy_search(index: &Index, query: &str) -> tantivy::Result<Vec<NodeSearchMeta>> {
+pub fn fuzzy_search(index: &Index, query: &str) -> tantivy::Result<Vec<NodeSearchLocation>> {
     let reader = index
         .reader_builder()
         .reload_policy(ReloadPolicy::OnCommit)
@@ -119,14 +106,14 @@ pub fn fuzzy_search(index: &Index, query: &str) -> tantivy::Result<Vec<NodeSearc
     let top_docs = searcher
         .search(&query, &(TopDocs::with_limit(MAX_SEARCH_RESULTS)))
         .unwrap();
-    let ids: Vec<NodeSearchMeta> = top_docs
+    let ids: Vec<NodeSearchLocation> = top_docs
         .iter()
         .map(|(_, doc_address)| {
             let doc = searcher.doc(*doc_address).unwrap();
             let doc_id_value = doc.get_first(doc_id_field).unwrap();
             let tab_id_value = doc.get_first(tab_id_field).unwrap();
             let node_id_value = doc.get_first(node_id_field).unwrap();
-            NodeSearchMeta {
+            NodeSearchLocation {
                 doc_id: Uuid::parse_str(doc_id_value.as_text().unwrap()).unwrap(),
                 tab_id: Uuid::parse_str(tab_id_value.as_text().unwrap()).unwrap(),
                 node_id: Uuid::parse_str(node_id_value.as_text().unwrap()).unwrap(),
@@ -156,7 +143,7 @@ mod tests {
         let text2 = "banana";
         update_search_index(
             &index,
-            NodeSearchMeta {
+            NodeSearchLocation {
                 doc_id: id1,
                 tab_id: Uuid::new_v4(),
                 node_id: Uuid::new_v4(),
@@ -166,7 +153,7 @@ mod tests {
         .unwrap();
         update_search_index(
             &index,
-            NodeSearchMeta {
+            NodeSearchLocation {
                 doc_id: id2,
                 tab_id: Uuid::new_v4(),
                 node_id: Uuid::new_v4(),
@@ -186,30 +173,6 @@ mod tests {
             .close()
             .expect("Failed to remove temporary directory");
     }
-    
-    #[test]
-    fn test_clear_node() {
-        // Create a temporary directory for the index
-        let temp_dir = TempDir::new().expect("Failed to create temporary directory");
-
-        // Initialize the index using the temporary directory
-        let index = initialize_search_index(temp_dir.path().to_path_buf());
-        let node_id = Uuid::new_v4();
-        let text = "example text";
-        update_search_index(&index, NodeSearchMeta { doc_id: Uuid::new_v4(), tab_id: Uuid::new_v4(), node_id }, text).unwrap();
-
-        // Clear the node from the index
-        clear_node_index(&index, &node_id).unwrap();
-
-        // Perform a search and assert that the node is not found
-        let query = "example";
-        let result = fuzzy_search(&index, query).unwrap();
-
-        assert_eq!(result.len(), 0);
-
-        // Clean up the temporary directory
-        temp_dir.close().expect("Failed to remove temporary directory");
-    }
 
     #[test]
     fn test_clear_tab() {
@@ -222,8 +185,8 @@ mod tests {
         let tab_id = Uuid::new_v4();
         let text_1 = "example text 1";
         let text_2 = "example text 2";
-        update_search_index(&index, NodeSearchMeta { doc_id, tab_id, node_id: Uuid::new_v4() }, text_1).unwrap();
-        update_search_index(&index, NodeSearchMeta { doc_id, tab_id, node_id: Uuid::new_v4() }, text_2).unwrap();
+        update_search_index(&index, NodeSearchLocation { doc_id, tab_id, node_id: Uuid::new_v4() }, text_1).unwrap();
+        update_search_index(&index, NodeSearchLocation { doc_id, tab_id, node_id: Uuid::new_v4() }, text_2).unwrap();
 
         // Clear the tab from the index
         clear_tab_index(&index, &tab_id).unwrap();
@@ -248,8 +211,8 @@ mod tests {
         let doc_id = Uuid::new_v4();
         let text_1 = "example text 1";
         let text_2 = "example text 2";
-        update_search_index(&index, NodeSearchMeta { doc_id, tab_id: Uuid::new_v4(), node_id: Uuid::new_v4() }, text_1).unwrap();
-        update_search_index(&index, NodeSearchMeta { doc_id, tab_id: Uuid::new_v4(), node_id: Uuid::new_v4() }, text_2).unwrap();
+        update_search_index(&index, NodeSearchLocation { doc_id, tab_id: Uuid::new_v4(), node_id: Uuid::new_v4() }, text_1).unwrap();
+        update_search_index(&index, NodeSearchLocation { doc_id, tab_id: Uuid::new_v4(), node_id: Uuid::new_v4() }, text_2).unwrap();
 
         // Clear the document from the index
         clear_doc_index(&index, &doc_id).unwrap();
