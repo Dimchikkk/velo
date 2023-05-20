72c019e2f1e4b1345b9abab941f3c5a9de10dd5c
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-05-20 09:06:57 +0100

    remove external crates from repo

--- crates/bevy_pkv/build.rs
+++
@@ -1,9 +0,0 @@
-use cfg_aliases::cfg_aliases;
-
-fn main() {
-    cfg_aliases! {
-        wasm: { target_arch = "wasm32" },
-        rocksdb_backend: { all(feature = "rocksdb", not(wasm)) },
-        sled_backend: { all(feature = "sled", not(wasm)) }
-    }
-}
