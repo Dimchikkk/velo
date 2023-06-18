b0291be3b39fc4593bd543a4d16fcd947bf81e27
Dima Rets <dmitryrets+1@gmail.com>
2023-06-18 02:00:17 +0100

    priority for modal
    Former-commit-id: a51f26be4815c3057dbf8fdf07a21a418703914a

--- src/ui_plugin/systems/update_rectangle_position.rs
+++ src/ui_plugin/systems/update_rectangle_position.rs
@@ -14,13 +14,16 @@ pub fn update_rectangle_position(
     mut velo_node_query: Query<&mut Transform, With<VeloNode>>,
     mut events: EventWriter<RedrawArrow>,
     camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    state: Res<UiState>,
+    ui_state: Res<UiState>,
 ) {
     let (camera, camera_transform) = camera_q.single();
     if !cursor_moved_events.is_empty() {
         let event = cursor_moved_events.iter().last().unwrap();
         for (raw_text, parent) in &mut raw_text_query.iter() {
-            if Some(raw_text.id) == state.hold_entity && state.entity_to_edit.is_none() {
+            if ui_state.modal_id.is_none()
+                && Some(raw_text.id) == ui_state.hold_entity
+                && ui_state.entity_to_edit.is_none()
+            {
                 if let Some(pos) = camera.viewport_to_world_2d(camera_transform, event.position) {
                     let border = border_query.get(parent.get()).unwrap();
                     let mut top = velo_node_query.get_mut(border.get()).unwrap();
