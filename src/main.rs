c40478d37155b1adf57cda5cfdcb265b355394e1
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-07-16 10:16:55 +0100

    enable rendering warning back

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     console_error_panic_hook::set_once();
     #[cfg(not(target_arch = "wasm32"))]
-    std::env::set_var("RUST_LOG", "warn,velo=info,tantivy=warn,bevy_render=off");
-    // bevy_render=off until https://github.com/johanhelsing/bevy_smud/issues/26 is fixed ^^
+    std::env::set_var("RUST_LOG", "warn,velo=info,tantivy=warn");
     App::new().add_plugins(VeloPlugin).run();
 }
