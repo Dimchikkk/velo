62f68c2b1684c411dcb869b1f812c2001cccf88e
StaffEngineer <velo.app1@gmail.com>
2023-07-18 14:02:20 +0100

    use sdf

--- src/canvas/grid/systems.rs
+++ src/canvas/grid/systems.rs
@@ -18,10 +18,18 @@ pub fn grid(
         },
         material: materials.add(CustomGridMaterial {
             color: Color::rgba(245. / 255., 245. / 255., 245. / 255., 1.),
-            line_color: Color::rgba(97. / 255., 164. / 255., 255. / 255., 0.5),
+            line_color: Color::rgba(97. / 255., 164. / 255., 255. / 255., 0.2),
             grid_size: size,
             cell_size: Vec2::new(10.0, 10.0),
         }),
         ..Default::default()
     });
+}
+
+pub fn update_camera_translation(
+    mut query: Query<&mut Transform, (Changed<Transform>, With<OrthographicProjection>)>,
+) {
+    for mut transform in query.iter_mut() {
+        transform.translation = transform.translation.round();
+    }
 }
