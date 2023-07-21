3d4a91c69d89ff21e55d278838d3fb2f07a39ab9
StaffEngineer <velo.app1@gmail.com>
2023-07-21 16:13:40 +0100

    remove changing color on note hover

--- src/ui_plugin/systems/button_handlers.rs
+++ src/ui_plugin/systems/button_handlers.rs
@@ -695,25 +695,18 @@ pub fn import_from_url(
 
 pub fn button_generic_handler(
     mut generic_button_query: Query<
-        (&Interaction, &mut BackgroundColor, Entity),
+        (&Interaction, Entity),
         (Changed<Interaction>, With<GenericButton>),
     >,
     mut windows: Query<&mut Window, With<PrimaryWindow>>,
     mut tooltips_query: Query<(&mut Style, &Parent), With<Tooltip>>,
 ) {
     let mut primary_window = windows.single_mut();
-    let delta = 0.2;
-    for (interaction, mut bg_color, entity) in &mut generic_button_query.iter_mut() {
+    for (interaction, entity) in &mut generic_button_query.iter_mut() {
         match *interaction {
             Interaction::Pressed => {}
             Interaction::Hovered => {
                 primary_window.cursor.icon = CursorIcon::Hand;
-                bg_color.0 = Color::rgba(
-                    bg_color.0.r(),
-                    bg_color.0.g(),
-                    bg_color.0.b(),
-                    bg_color.0.a() - delta,
-                );
                 for (mut style, parent) in tooltips_query.iter_mut() {
                     if parent.get() == entity {
                         style.display = Display::Flex;
@@ -722,12 +715,6 @@ pub fn button_generic_handler(
             }
             Interaction::None => {
                 primary_window.cursor.icon = CursorIcon::Default;
-                bg_color.0 = Color::rgba(
-                    bg_color.0.r(),
-                    bg_color.0.g(),
-                    bg_color.0.b(),
-                    bg_color.0.a() + delta,
-                );
                 for (mut style, parent) in tooltips_query.iter_mut() {
                     if parent.get() == entity {
                         style.display = Display::None;
